54dcff3f3430302514f3c7cb40cae4be3fb2c8f1
daniaandreea <chitandania@yahoo.com>
2021-10-20 16:57:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
 
+
+def read_list():
+    given = input('Dați numerele separate prin virgulă: ')
+    str_list = given.split(',')
+    int_list = []
+    for str_num in str_list:
+        int_list.append(int(str_num))
+    return int_list
+
+
+def print_menu():
+    print('1. Citire date.')
+    print('2. Determinare cea mai lungă subsecvență cu numere prime.')
+    print('3. Determinare cea mai lungă subsecvență de numere cu același număr de divizori.')
+    print('4. Determinare cea mai lungă subsecvență de palindroame.')
+    print('x. Ieșire.')
+
+
+def main():
+    lst = []
+    while True:
+        print_menu()
+        option = input('Alegeți opțiunea: ')
+        if option == '1':
+            lst = read_list()
+        elif option == '2':
+            result = get_longest_sublist_all_prime(lst)
+            print('Cea mai lungă subsecvență cu toate numerele prime este: ')
+            print(result)
+        elif option == '3':
+            result = get_longest_sublist_all_equal_nr_of_divisors(lst)
+            print('Cea mai lungă subsecvență formată doar din elemente cu același număr de divizori este: ')
+            print(result)
+        elif option == '4':
+            result = get_longest_sublist_all_palindrome(lst)
+            print('Cea mai lungă subsecvență cu palindroame este: ')
+            print(result)
+        elif option == 'x':
+            break
+        else:
+            print('Opțiune invalidă, reîncearcă!')
+
+if __name__ == '__main__':
+    main()
