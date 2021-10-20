8a749ad7d5d88fa02b742ed443022b524fbb93a5
daniaandreea <chitandania@yahoo.com>
2021-10-20 18:51:35 +0300

    f

--- main.py
+++ main.py
@@ -8,10 +8,28 @@ def read_list():
         int_list.append(int(str_num))
     return int_list
 
+def get_negatives(elem):
+    '''
+    Functia afiseaza numerele negative din lista.
+    in: lista data de utilizator
+    out: lista formata din numere negative
+    '''
+    result=[]
+    for elem in lst:
+        if elem < 0:
+            result.append(elem)
+        else:
+            elem=elem+1
+    return result
+
+def test_get_negatives():
+    assert get_negatives([-1,2,3,-14]) == [-1,-14]
+    assert get_negatives([3,6,8,9]) ==[]
+    assert get_negatives([])==[]
 
 def print_menu():
-    print('1. Citire date.')
-    print('2. Determinare cea mai lungă subsecvență cu numere prime.')
+    print('1. Citirea unei liste de numere intregi.')
+    print('2. Afisarea tuturor numerelor negative nenule din lista.')
     print('3. Determinare cea mai lungă subsecvență de numere cu același număr de divizori.')
     print('4. Determinare cea mai lungă subsecvență de palindroame.')
     print('x. Ieșire.')
@@ -25,7 +43,7 @@ def main():
         if option == '1':
             lst = read_list()
         elif option == '2':
-            result = get_longest_sublist_all_prime(lst)
+            result = get_negatives(lst)
             print('Cea mai lungă subsecvență cu toate numerele prime este: ')
             print(result)
         elif option == '3':
